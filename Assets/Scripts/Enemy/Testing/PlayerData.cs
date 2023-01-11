30bbfa6e58e51717369ec88746a9736e5669a922
Aurora <78513080+CloudOni@users.noreply.github.com>
2023-01-11 09:28:47 +0100

    Moved PlayerContent to it's own folder

--- Assets/Scripts/Enemy/Testing/PlayerData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerData", menuName = "Player/PlayerPos")]
-public class PlayerData : ScriptableObject
-{
-    public Vector3 position;
-}
