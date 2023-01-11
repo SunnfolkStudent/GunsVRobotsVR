b9df90668b41dcce0835733b1f19a8f363cd1929
Nils <nilsmorkstene@gmail.com>
2023-01-11 11:10:51 +0100

    Gunslinger AI

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PlayerData", menuName = "Player/PlayerPos")]
+[CreateAssetMenu(fileName = "Player/PlayerData", menuName = "Player/PlayerPos")]
 public class PlayerData : ScriptableObject
 {
     public Vector3 position;
