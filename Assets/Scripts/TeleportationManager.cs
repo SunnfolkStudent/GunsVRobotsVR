57e0503a492c256c8dcf7fa0473a8e0608f09caa
Teffle <Tellefsplass@hotmail.no>
2023-01-11 11:17:42 +0100

    WIP TeleportManager

--- Assets/Scripts/TeleportationManager.cs
+++ Assets/Scripts/TeleportationManager.cs
@@ -1,10 +1,28 @@
+using System;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class TeleportationManager : MonoBehaviour
 {
-    private void Update()
+    public GameObject BaseControllerGameObject;
+    public GameObject teleportationGameObject;
+
+    public InputActionReference teleportActivationReferance;
+
+    public UnityEvent onTeleportActivate;
+    public UnityEvent onTeleportCancel;
+
+    private void Start()
+    {
+        teleportActivationReferance.action.performed += TeleportModeActivate;
+        teleportActivationReferance.action.canceled += TeleportModeCancel;
+    }
+
+    private void TeleportModeCancel(InputAction.CallbackContext obj)
     {
         
     }
+
+    private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
 }
