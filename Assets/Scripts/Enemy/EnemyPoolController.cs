e38b286899d6e6e3eaa71bad598f90cd8cb5f259
Nils <nilsmorkstene@gmail.com>
2023-01-12 10:07:19 +0100

    Mer fiksing på events for lyd

--- Assets/Scripts/Enemy/EnemyPoolController.cs
+++ Assets/Scripts/Enemy/EnemyPoolController.cs
@@ -10,9 +10,12 @@ public class EnemyPoolController : MonoBehaviour
     public List<GameObject> inactiveEnemies;
     public List<GameObject> activeEnemies;
     public GameObject enemyPrefab;
+    public PlayerAudio audio;
 
     private void Awake()
     {
+        EnemyPoolController.CurrentEnemyPoolController.audio = this;
+        
         if (CurrentEnemyPoolController)
         {
             Destroy(this);
@@ -24,22 +27,26 @@ public class EnemyPoolController : MonoBehaviour
 
     public void SpawnEnemy(EnemyStats enemyStats, Vector3 position, Quaternion rotation)
     {
+        EnemyStateManager stateManager;
         GameObject enemy;
         if (inactiveEnemies.Count != 0)
         {
             enemy = inactiveEnemies[0];
             inactiveEnemies.RemoveAt(0);
+            stateManager = enemy.GetComponent<EnemyStateManager>();
         }
         else
         {
             enemy = Instantiate(enemyPrefab);
+            stateManager = enemy.GetComponent<EnemyStateManager>();
+            stateManager.OnEnemyHit.AddListener(audio.OnEnemyHit);
+            stateManager.OnEnemyDeath.AddListener(audio.OnEnemyKill);
         }
         
         activeEnemies.Add(enemy);
 
         enemy.transform.position = position;
         enemy.transform.rotation = rotation;
-        var stateManager = enemy.GetComponent<EnemyStateManager>();
         
         stateManager.enemyStats = enemyStats;
         stateManager.currentState = stateManager.InitialiseState;
