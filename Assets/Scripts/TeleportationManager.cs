1f740c0a482e7ef00d409984c52c6e82b4213ce4
Teffle <Tellefsplass@hotmail.no>
2023-01-12 14:35:38 +0100

    Teleportation keybinds work

--- Assets/Scripts/TeleportationManager.cs
+++ Assets/Scripts/TeleportationManager.cs
@@ -1,15 +1,18 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
 
 public class TeleportationManager : MonoBehaviour
 {
     public GameObject BaseControllerGameObject;
     public GameObject teleportationGameObject;
-
+    
     public InputActionReference teleportActivationReferance;
+    public InputActionReference onTeleport;
+    public Image image;
 
+    [Space]
     public UnityEvent onTeleportActivate;
     public UnityEvent onTeleportCancel;
 
@@ -17,12 +20,19 @@ public class TeleportationManager : MonoBehaviour
     {
         teleportActivationReferance.action.performed += TeleportModeActivate;
         teleportActivationReferance.action.canceled += TeleportModeCancel;
+        onTeleport.action.performed += Darktime;
+        image = GetComponent<Color>();
+        var color = image
     }
 
-    private void TeleportModeCancel(InputAction.CallbackContext obj)
+    private void Darktime(InputAction.CallbackContext obj)
     {
         
     }
 
-    private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
+    private void TeleportModeActivate(InputAction.CallbackContext obj) => Invoke("DeactivateTeleporter", .1f);
+
+    void DeactivateTeleporter() => onTeleportCancel.Invoke();
+
+    private void TeleportModeCancel(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
 }
