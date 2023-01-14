5c602b635cee425c926fb07c9eb6801170a547d0
Teffle <Tellefsplass@hotmail.no>
2023-01-14 15:48:39 +0100

    Added XR Rig with hand switch and fade on teleport bool

---
+++ Assets/HandController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    [Header("Teleport Hand Configuration")]
+    [Space]
+    
+    [Tooltip("True = LefHandTeleport, False = RightHandTeleport")]
+    public bool teleportHand;
+    
+    [Tooltip("Keep Empty in Editor")]
+    public GameObject currentTeleportHand;
+    
+    public GameObject leftHandTeleportController;
+    public GameObject rightHandTeleportController;
+
+    private void Start()
+    {
+        UpdateHands();
+    }
+
+    private void Update()
+    {
+        if (currentTeleportHand == null)
+        {
+            UpdateHands();
+        }
+    }
+
+    private void UpdateHands() //Call on this function when player switches teleport hand
+    {
+        if (teleportHand)
+        {
+            //Set Teleport Hand to Left Hand
+            leftHandTeleportController.SetActive(true);
+            rightHandTeleportController.SetActive(false);
+            currentTeleportHand = leftHandTeleportController;
+        }
+        else
+        {
+            //Set Teleport Hand to Right Hand
+            rightHandTeleportController.SetActive(true);
+            leftHandTeleportController.SetActive(false);
+            currentTeleportHand = rightHandTeleportController;
+        }
+    }
+}
--- Assets/Scripts/TeleportationManager.cs
+++ Assets/Scripts/TeleportationManager.cs
@@ -7,15 +7,10 @@ using UnityEngine.Events;
 
 public class TeleportationManager : MonoBehaviour
 {
-    public ScreenFade screenFade;
-
-    public float teleportCooldown = 2f;
-    
     public GameObject BaseControllerGameObject;
     public GameObject teleportationGameObject;
     
     public InputActionReference teleportActivationReferance;
-    public InputActionReference teleportCommitReferance;
 
     [Space]
     public UnityEvent onTeleportActivate;
@@ -26,28 +21,11 @@ public class TeleportationManager : MonoBehaviour
         teleportActivationReferance.action.performed += TeleportModeActivate;
         teleportActivationReferance.action.canceled += TeleportModeCancel;
     }
-
-    private void Update()
-    {
-        if (teleportCommitReferance.action.WasPressedThisFrame() && screenFade.fadeColor.a < 1)
-        {
-            //StartCoroutine(TeleportFade());
-        }
-    }
+    
 
     private void TeleportModeActivate(InputAction.CallbackContext obj) => Invoke("DeactivateTeleporter", .1f);
 
-    void DeactivateTeleporter() => onTeleportCancel.Invoke();
+    public void DeactivateTeleporter() => onTeleportCancel.Invoke();
 
     private void TeleportModeCancel(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
-
-    IEnumerator TeleportFade()
-    {
-        screenFade.fadeDuration = 0.1f;
-        screenFade.FadeOut();
-        yield return new WaitForSeconds(screenFade.fadeDuration);
-        screenFade.fadeDuration = 1f;
-        screenFade.FadeIn();
-        yield return new WaitForSeconds(screenFade.fadeDuration);
-    }
 }
