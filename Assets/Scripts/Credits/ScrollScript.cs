63ace81555abc9497dcdb148cadcd63673a4da98
Teffle <Tellefsplass@hotmail.no>
2023-01-18 14:42:31 +0100

    Test scene Teleport

--- Assets/Scripts/Credits/ScrollScript.cs
+++ Assets/Scripts/Credits/ScrollScript.cs
@@ -11,20 +11,27 @@ public class ScrollScript : MonoBehaviour
     private Vector3 _startPosition;
     [SerializeField]
     private Vector3 _target;
+    [SerializeField]
     private float _timeToReachTarget;
+    [SerializeField]
+    private Vector3 distance;
+    public AnimationCurve curve;
     void Start()
     {
         _startPosition = transform.position;
         // Making target resolution independent
         var rectTransform = GetComponentInChildren<RectTransform>();
-        _target = new Vector3(_startPosition.x, 200, 100);
+        _target = new Vector3(_startPosition.x + distance.x, _startPosition.y + distance.y, _startPosition.z + distance.z);
         SetDestination();
     }
     void Update()
     {
         _time += Time.deltaTime / _timeToReachTarget;
-        transform.position = Vector3.Lerp(_startPosition, _target, _time);
-
+        Vector3 position = transform.position;
+        position = Vector3.Lerp(_startPosition, _target, curve.Evaluate(_time));
+        //position.z = zDistance * ;
+        transform.position = position;    
+        
         if (Keyboard.current.anyKey.isPressed)
         {
             _timeToReachTarget = 5f;
---
+++ Assets/Scripts/Player/XRRig/ForceGrab.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ForceGrab : MonoBehaviour
+{
+    public XRGrabInteractable grabInteractable;
+
+    private void Start()
+    {
+        
+    }
+}
