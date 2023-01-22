3d829c9d7e469447dec6072815fc15f3cad8f61a
Nils <nilsmorkstene@gmail.com>
2023-01-22 11:07:24 +0100

    Update WristUIController.cs

--- Assets/Scripts/Player/WristUIController.cs
+++ Assets/Scripts/Player/WristUIController.cs
@@ -8,23 +8,29 @@ using UnityEngine.UI;
 public class WristUIController : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI numbers;
-    [SerializeField] private Image symbol;
-    [SerializeField] private Image healthBar;
+    [SerializeField] private GameObject healthBar;
     
     [SerializeField] private PlayerHealthManager _playerHealthManager;
     [SerializeField] private WeaponMain _weaponMain;
 
-    [SerializeField] private Sprite _bulletSymbol;
-    [SerializeField] private Sprite _percentSymbol;
+    [SerializeField] private GameObject _bulletSymbol;
+    [SerializeField] private GameObject _percentSymbol;
 
     private void Update()
     {
-        healthBar.fillAmount = Mathf.Clamp(_playerHealthManager.CurrentIntegrity / _playerHealthManager.maxIntegrity, 0f, 1f);
+        var fractionOfHealth = Mathf.Clamp(_playerHealthManager.CurrentIntegrity / _playerHealthManager.maxIntegrity, 0f, 1f);
+
+        healthBar.transform.localScale = new Vector3(0f, fractionOfHealth, 0f);
+
+        var localPosition = healthBar.transform.localPosition;
+        healthBar.transform.localPosition =
+            new Vector3(Mathf.Lerp(0f, 0.00125f, fractionOfHealth), localPosition.y, localPosition.z);
         
         if (_weaponMain.isSwap)
         {
-            symbol.sprite = _percentSymbol;
-            
+            _percentSymbol.SetActive(true);
+            _bulletSymbol.SetActive(false);
+
             var fractionOfSwapTime =
                 Mathf.Clamp((Time.time - _weaponMain.swapTimer) / _weaponMain.gunData.swapTime, 0f, 1f);
             var percentage = Mathf.RoundToInt(fractionOfSwapTime * 100f);
@@ -33,7 +39,8 @@ public class WristUIController : MonoBehaviour
         }
         else
         {
-            symbol.sprite = _bulletSymbol;
+            _bulletSymbol.SetActive(true);
+            _percentSymbol.SetActive(false);
             
             numbers.text = _weaponMain.gunData.currentAmmo.ToString("D3");
         }
