eecd84ec734e0e3807661c40dbb6b0a8103bd0a2
Nils <nilsmorkstene@gmail.com>
2023-01-22 20:33:43 +0100

    Fiender stopper når de skyter

--- Assets/Scripts/Enemy/EnemyPerformAttackState.cs
+++ Assets/Scripts/Enemy/EnemyPerformAttackState.cs
@@ -6,17 +6,16 @@ public class EnemyPerformAttackState : EnemyBaseState
 {
     public override void EnterState(EnemyStateManager enemy)
     {
-        enemy.agent.speed = 0f;
+        enemy.agent.isStopped = true;
         
         enemy.animator.Play("AimUp");
-        
-        enemy.SwitchState(enemy.EngageState);
     }
 
     public override void HandleState(EnemyStateManager enemy)
     {
         if (enemy.animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
         {
+            enemy.agent.isStopped = false;
             enemy.SwitchState(enemy.EngageState);
         }
     }
