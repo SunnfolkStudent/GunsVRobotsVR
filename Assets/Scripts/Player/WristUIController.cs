ab808ff19747e2eb22d35ceffe8e05fe47289ad8
Nils <nilsmorkstene@gmail.com>
2023-01-23 12:41:22 +0100

    Left hand

--- Assets/Scripts/Player/WristUIController.cs
+++ Assets/Scripts/Player/WristUIController.cs
@@ -8,22 +8,25 @@ using UnityEngine.UI;
 public class WristUIController : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI numbers;
-    [SerializeField] private Image symbol;
-    [SerializeField] private Image healthBar;
+    [SerializeField] private GameObject healthBar;
     
     [SerializeField] private PlayerHealthManager _playerHealthManager;
     [SerializeField] private WeaponMain _weaponMain;
 
-    [SerializeField] private Sprite _bulletSymbol;
-    [SerializeField] private Sprite _percentSymbol;
-
+    [SerializeField] private GameObject _bulletSymbol;
+    [SerializeField] private GameObject _percentSymbol;
+    
     private void Update()
     {
-        healthBar.fillAmount = Mathf.Clamp(_playerHealthManager.CurrentIntegrity / _playerHealthManager.maxIntegrity, 0f, 1f);
+        var fillAmount = Mathf.Clamp(_playerHealthManager.CurrentIntegrity / _playerHealthManager.maxIntegrity, 0f, 1f);
+        
+        healthBar.transform.localScale = new Vector3(1f, fillAmount, 1f);
+        healthBar.transform.localPosition = new Vector3(0f, -0.00212f * (1f - fillAmount), 0f);
         
         if (_weaponMain.isSwap)
         {
-            symbol.sprite = _percentSymbol;
+            _percentSymbol.SetActive(true);
+            _bulletSymbol.SetActive(false);
             
             var fractionOfSwapTime =
                 Mathf.Clamp((Time.time - _weaponMain.swapTimer) / _weaponMain.gunData.swapTime, 0f, 1f);
@@ -33,7 +36,8 @@ public class WristUIController : MonoBehaviour
         }
         else
         {
-            symbol.sprite = _bulletSymbol;
+            _bulletSymbol.SetActive(true);
+            _percentSymbol.SetActive(false);
             
             numbers.text = _weaponMain.gunData.currentAmmo.ToString("D3");
         }
