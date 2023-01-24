7a8c0f71d7b388adafb480942b741a697de53091
Nils <nilsmorkstene@gmail.com>
2023-01-24 14:09:01 +0100

    Eksplosjon når fiendene dør

--- Assets/Scripts/Enemy/EnemyPoolController.cs
+++ Assets/Scripts/Enemy/EnemyPoolController.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.VFX;
 
 public class EnemyPoolController : MonoBehaviour
 {
     public static EnemyPoolController CurrentEnemyPoolController;
     
     public List<GameObject> activeEnemies;
+    public GameObject explosionPrefab;
 
     private void Awake()
     {
@@ -31,6 +34,11 @@ public class EnemyPoolController : MonoBehaviour
     public void DestroyEnemy(GameObject enemyToDestroy)
     {
         activeEnemies.Remove(enemyToDestroy);
+        var explosion = Instantiate(explosionPrefab, enemyToDestroy.transform.position, Quaternion.identity);
+        if (enemyToDestroy.TryGetComponent<EnemyStateManager>(out var enemyStateManager))
+        {
+            explosion.transform.localScale = new Vector3(2f, 2f, 2f);
+        }
         Destroy(enemyToDestroy);
     }
 }
