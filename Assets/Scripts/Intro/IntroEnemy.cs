19979f5f478fc1c61ead5e75ad21459eb048e700
Nils <nilsmorkstene@gmail.com>
2023-01-25 15:12:19 +0100

    Fiende sprenger i hovedmeny

--- Assets/Scripts/Intro/IntroEnemy.cs
+++ Assets/Scripts/Intro/IntroEnemy.cs
@@ -7,10 +7,13 @@ public class IntroEnemy : MonoBehaviour
 {
     [SerializeField] private GameObject _nextLevelTriggerPrefab;
     [SerializeField] private Vector3 _spawnPosition;
+    [SerializeField] private GameObject _explosionPrefab;
     
     private void OnTriggerEnter(Collider other)
     {
+        Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
         Instantiate(_nextLevelTriggerPrefab, _spawnPosition, Quaternion.identity);
         GameObject.Find("IntroSceneObject").GetComponent<IntroScene>().OnShootEnemy();
+        Destroy(gameObject);
     }
 }
